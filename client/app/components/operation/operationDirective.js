e2fb082e61de9038c2bc7fcd770cf61aefe21f2f
Jordan Aupetit <aupetit.jordan@gmail.com>
2015-02-25 19:58:28 +0100

    MAJ Add operation -- NOT a modal

--- client/app/components/operation/operationDirective.js
+++ client/app/components/operation/operationDirective.js
@@ -25,4 +25,30 @@
             };
         })
 
+        .directive('btnAddOperation', function () {
+            return {
+                restrict: 'AE',
+                replace: false,
+                transclude: false,
+                link: function (scope, element, attrs) {
+                    $(element).click(function() {
+                        $(".menuCreateOperation").animate({ "right": "0" }, "slow" );
+                    });
+                }
+            };
+        })
+
+        .directive('closeMenuCreateOperation', function () {
+            return {
+                restrict: 'AE',
+                replace: false,
+                transclude: false,
+                link: function (scope, element, attrs) {
+                    $(element).click(function() {
+                        $(".menuCreateOperation").animate({ "right": "-300px" }, "slow" );
+                    });
+                }
+            };
+        })
+
 })();
