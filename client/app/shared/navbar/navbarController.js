7cdc5455364197954ede40b75d359e80d1abe3f8
Jordan Aupetit <aupetit.jordan@gmail.com>
2015-03-06 13:14:48 +0100

    Few correctifs of navbar

--- client/app/shared/navbar/navbarController.js
+++ client/app/shared/navbar/navbarController.js
@@ -3,24 +3,33 @@
 'use strict';
 
     angular
-
         .module('controllers')
-        .controller('NavbarController', ['$scope','$rootScope','$state','ipCookie', function($scope,$rootScope,$state,ipCookie) {
-                $rootScope.currentUserSignedIn = ipCookie('token');
-                $rootScope.utlisateurCourant = ipCookie('user');
-           $scope.logout = function() {
+        .controller('NavbarController', ['$scope','$rootScope','$state','ipCookie', 'AccountResource', function($scope, $rootScope, $state, ipCookie, AccountResource) {
+            
+
+            $rootScope.currentUserSignedIn = ipCookie('token');
+            $rootScope.utlisateurCourant = ipCookie('user');
+           
+            $scope.logout = function() {
                 ipCookie.remove('token');
                 ipCookie.remove('user');
                 $rootScope.currentUserSignedIn = null;
                 $rootScope.utlisateurCourant = null ;
                 $state.go('login');
 
-
             }, function() {
                 alert("Failed to logout!");
             
             };
+
+            var getAccounts = function() {
+                AccountResource.getAll().$promise.then(function(accounts){
+                    $scope.accounts = accounts
+                })
+            }
+
+            getAccounts();
         
-}]);
+        }]);
 	
 })();
