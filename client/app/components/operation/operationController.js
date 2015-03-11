7bf454a5e158d2da44172c0a20a3db267add5a3b
Jordan Aupetit <aupetit.jordan@gmail.com>
2015-03-11 22:50:03 +0100

    Correction bug balance / deferred balance

--- client/app/components/operation/operationController.js
+++ client/app/components/operation/operationController.js
@@ -162,6 +162,9 @@
                         $scope.operations = operations
                         StorageServices.setOperations(accountId, operations)
                         genCategories()
+
+                        // Le fix doit se faire avant l'update
+                        fixOperations()
                         $scope.updateSolde()
 
 
